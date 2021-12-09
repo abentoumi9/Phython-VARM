d6a477008ab4e4aeec14e09eb7e7caab9a73e2bf
Martijn <margroenendijk@deloitte.nl>
2021-12-09 14:45:11 +0100

    API Time series daily

--- Phyton VARM.py
+++ Phyton VARM.py
@@ -1,2 +1,15 @@
-Hi VARMers
-print('Wiehetlaatstlachtlachthetbest')
+
+
+#API connection
+
+import requests
+import pandas as pd
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=53HGXB28Z5ZZKI9S'
+r = requests.get(url)
+data = r.json()
+
+print(data)
+
+data.keys()
+
